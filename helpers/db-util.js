45835e51cf80831f7534c97a881a2cc5cf0c4509
Mohamed Ramadan <mrbp25091993@gmail.com>
2024-03-26 20:51:56 +0200

    Add secrets to .env file

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export async function connectDB() {
   const client = await MongoClient.connect(
-    "mongodb+srv://MoRam:jNrm1bwQrU9CZOtS@cluster0.avkqief.mongodb.net/events?retryWrites=true&w=majority&appName=Cluster0"
+    `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_cluster_name}.avkqief.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority&appName=Cluster0`
   );
 
   return client;
